130847c5614f4d5c309c1135f2450a4015940227
manizm <iam.naeemmi.work@gmail.com>
2021-12-20 15:37:11 +0100

    changed cache store to use Map instead of pojo

--- src/lib/CacheFactory.ts
+++ src/lib/CacheFactory.ts
@@ -1,13 +1,21 @@
 import {CacheStore} from './CacheStore';
+import {NotFound} from './common/errors/NotFound';
 
 export class CacheFactory {
-  static #stores: {[key: string]: CacheStore<unknown, unknown>};
-
+  static #stores: Map<string, CacheStore<unknown, unknown>>;
   static addStore(key: string, store: CacheStore<unknown, unknown>) {
-    this.#stores[key] = store;
+    this.#stores.set(key, store);
   }
 
   static getStore(key: string): CacheStore<unknown, unknown> {
-    return this.#stores[key];
+    const store = this.#stores.get(key);
+
+    if (!store) throw new NotFound('Cache Store does not exist');
+
+    return store;
+  }
+
+  static removeStore(key: string): boolean {
+    return this.#stores.delete(key);
   }
 }
