faf67c594b392d1239ba7ce5b2eea696f3f4dcbc
manizm <iam.naeemmi.work@gmail.com>
2021-12-20 19:02:58 +0100

    added validation for keys and a small type fix

--- src/lib/CacheFactory.ts
+++ src/lib/CacheFactory.ts
@@ -1,4 +1,6 @@
 import {CacheStore} from './CacheStore';
+import {DuplicateKey} from './common/errors/DuplicateKey';
+import {InvalidArgument} from './common/errors/InvalidArgument';
 import {NotFound} from './common/errors/NotFound';
 
 export class CacheFactory {
@@ -9,10 +11,23 @@ export class CacheFactory {
   }
 
   addStore(key: string, store: CacheStore<unknown, unknown>) {
+    if (typeof key !== 'string') {
+      throw new InvalidArgument('key must be a string for adding cache store');
+    }
+
+    // don't let the users implicitly override an added store
+    if (this.getKeys().includes(key)) {
+      throw new DuplicateKey(`key: ${key} already exists`);
+    }
+
     this.#stores.set(key, store);
   }
 
   getStore(key: string): CacheStore<unknown, unknown> {
+    if (typeof key !== 'string') {
+      throw new InvalidArgument('a valid key is required to get store');
+    }
+
     const store = this.#stores.get(key);
 
     if (!store) throw new NotFound('Cache Store does not exist');
@@ -20,11 +35,15 @@ export class CacheFactory {
     return store;
   }
 
-  getKeys(): unknown[] {
+  getKeys(): string[] {
     return [...this.#stores.keys()];
   }
 
   removeStore(key: string): boolean {
+    if (typeof key !== 'string') {
+      throw new InvalidArgument('a valid key is required to delete a store');
+    }
+
     return this.#stores.delete(key);
   }
 }
