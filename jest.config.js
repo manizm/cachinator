eccf9066c5ed3703831a7d22bcac093ec4ffede3
manizm <iam.naeemmi.work@gmail.com>
2022-06-08 00:52:48 +0200

    added dev.build folder to ignore list

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/', '/build/', '/example/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/build/',
+    '/example/',
+    '/dev.build/',
+  ],
 };
